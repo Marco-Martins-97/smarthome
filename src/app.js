db9a987eaeaa5badff098c3af6340bd6f13f350a
Marco Martins <marco.agm97+github@gmail.com>
2026-09-02 22:09:40 +0100

    project structure

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.status(200).send('Ok');
 });
 
-app.listen(3000, () => {
-    console.log('Server running at http://localhost:3000');
-});
+export default app;
